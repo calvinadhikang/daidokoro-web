31c2909a7f5d3bead2ab2f1f269e231424fa35ff
Calvin <calvinadhikang@gmail.com>
2026-07-10 10:01:23 +0700

    Add Omakase API routes for session and menu management
    - Introduced new routes under the 'omakase' prefix for managing Omakase sessions and menus.
    - Added endpoints for CRUD operations on Omakase sessions and menus in `api.php`.
    - Integrated `OmakaseController` to handle requests related to Omakase functionality.

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\MenuApiController;
 use App\Http\Controllers\Api\NotificationApiController;
 use App\Http\Controllers\Api\TransactionApiController;
+use App\Http\Controllers\OmakaseController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('menu')->group(function () {
@@ -25,6 +26,18 @@ Route::prefix('notification')->group(function () {
     Route::post('/register', [NotificationApiController::class, 'register']);
     Route::get('/test', [NotificationApiController::class, 'testBroadcast']);
     Route::post('/test', [NotificationApiController::class, 'test']);
+});
+
+Route::prefix('omakase')->group(function () {
+    Route::get('/session', [OmakaseController::class, 'getOmakaseSessions']);
+    Route::get('/session/{id}', [OmakaseController::class, 'getOmakaseSessionById']);
+    Route::post('/session/create', [OmakaseController::class, 'createOmakaseSession']);
+    Route::post('/session/update/{id}', [OmakaseController::class, 'updateOmakaseSession']);
+    Route::post('/session/delete/{id}', [OmakaseController::class, 'deleteOmakaseSession']);
+
+    Route::post('/menu/create', [OmakaseController::class, 'createOmakaseMenu']);
+    Route::post('/menu/delete/{id}', [OmakaseController::class, 'deleteOmakaseMenu']);
+    Route::post('/menu/update/{id}', [OmakaseController::class, 'updateOmakaseMenu']);
 });
 
 Route::get('/ping', function () {
